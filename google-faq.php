b06dbbd42b3709fae69033c1d70e1faefdad5120
cromeo98 <romeo.christian98@gmail.com>
2021-07-28 09:37:46 +0200

    other fix in indentation in array $frequently_aske

--- google-faq.php
+++ google-faq.php
@@ -64,7 +64,7 @@ Gestire il “Database” e la visualizzazione di queste domande e risposte con 
             'answer' => [
                 "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni."
             ]
-            ],
+        ],
         'faq6' => [
             'question' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
             'answer' => [
